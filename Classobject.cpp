ca777a686b6f0106361c0117153f45761560cc70
godwinthomas11 <136507258+godwinthomas11@users.noreply.github.com>
2024-11-11 01:26:01 +0530

    Update Classobject.cpp

--- Classobject.cpp
+++ Classobject.cpp
@@ -1,11 +1,20 @@
+// Develop an object oriented program in C++ to create a
+// database of student information systems containing the
+// following information: Name, Roll number, Class, division,
+// Date of Birth, Adhar number, Blood group, Contact address,
+// telephone number, etc . Count the number of students in
+// the database by using a variable count which is a member
+// of the student class. Also display the total number of
+// students using a static function.
+// Display the % marks obtained using friend function.
+
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Student {
 private:
     string name;
-    int rollNumber;
+    int rollNumber;s
     string studentClass;
     char division;
     string dob;
@@ -14,22 +23,38 @@ private:
     string contactAddress;
     string telephoneNumber;
     float percentageMarks;
-    static int count;  // Static member to keep track of the number of students
+    static int count;
 
 public:
-    // Constructor to initialize student data and increment count
-    Student(const string &n, int roll, const string &cls, char div, const string &birthdate, 
-            const string &aadhar, const string &blood, const string &address, 
-            const string &phone, float marks) 
-            : name(n), rollNumber(roll), studentClass(cls), division(div), dob(birthdate),
-              aadharNumber(aadhar), bloodGroup(blood), contactAddress(address), 
-              telephoneNumber(phone), percentageMarks(marks) {
-        count++;  // Increment count each time a new student is created
+    Student() {
+        count++;
     }
 
-    // Destructor to decrement count when a student is removed
-    ~Student() {
-        count--;
+    // Function to initialize student data
+    void setData(const string &n, int roll, const string &cls, char div, const string &birthdate, const string &aadhar, const string &blood, const string &address, const string &phone, float marks) {
+        name = n;
+        rollNumber = roll;
+        studentClass = cls;
+        division = div;
+        dob = birthdate;
+        aadharNumber = aadhar;
+        bloodGroup = blood;
+        contactAddress = address;
+        telephoneNumber = phone;
+        percentageMarks = marks;
+    }
+
+    // Static function to display student information
+    static void displayStudentInfo(const Student &student) {
+        cout << "Name: " << student.name << endl;
+        cout << "Roll Number: " << student.rollNumber << endl;
+        cout << "Class: " << student.studentClass << endl;
+        cout << "Division: " << student.division << endl;
+        cout << "Date of Birth: " << student.dob << endl;
+        cout << "Aadhar Number: " << student.aadharNumber << endl;
+        cout << "Blood Group: " << student.bloodGroup << endl;
+        cout << "Contact Address: " << student.contactAddress << endl;
+        cout << "Telephone Number: " << student.telephoneNumber << endl;
     }
 
     // Static function to get the count of students
@@ -37,34 +62,73 @@ public:
         return count;
     }
 
-    // Function to display student data
-    void display() const {
-        cout << "\nStudent Information:" << endl;
-        cout << "Name: " << name << endl;
-        cout << "Roll Number: " << rollNumber << endl;
-        cout << "Class: " << studentClass << endl;
-        cout << "Division: " << division << endl;
-        cout << "Date of Birth: " << dob << endl;
-        cout << "Aadhar Number: " << aadharNumber << endl;
-        cout << "Blood Group: " << bloodGroup << endl;
-        cout << "Contact Address: " << contactAddress << endl;
-        cout << "Telephone Number: " << telephoneNumber << endl;
-    }
-
     // Friend function to display percentage marks
-    friend void displayPercentage(const Student &student);
+    friend void displayPercentageMarks(const Student &student);
 };
 
-// Initialize the static member count
 int Student::count = 0;
 
-// Friend function to display percentage marks
-void displayPercentage(const Student &student) {
+void displayPercentageMarks(const Student &student) {
     cout << "Percentage Marks: " << student.percentageMarks << "%" << endl;
 }
 
 int main() {
-    // Creating student objects
-    Student student1("John Doe", 101, "10th", 'A', "2005-05-15", "123456789012", 
-                     "O+", "123 Main St, City", "555-1234", 88.5);
-    Student student2("Jane Smith", 102, "10th", 'B', "2005-08-20", "98765432109
+    const int numStudents = 2;
+    Student students[numStudents];
+
+    for (int i = 0; i < numStudents; ++i) {
+        string name, studentClass, dob, aadharNumber, bloodGroup, contactAddress, telephoneNumber;
+        int rollNumber;
+        char division;
+        float percentageMarks;
+
+        cout << "Enter details for student " << i + 1 << ":" << endl;
+
+        cout << "Name: ";
+        getline(cin, name);
+
+        cout << "Roll Number: ";
+        cin >> rollNumber;
+        cin.get(); // Consume the newline character left in the buffer
+
+        cout << "Class: ";
+        getline(cin, studentClass);
+
+        cout << "Division: ";
+        cin >> division;
+        cin.get(); // Consume the newline character left in the buffer
+
+        cout << "Date of Birth (YYYY-MM-DD): ";
+        getline(cin, dob);
+
+        cout << "Aadhar Number: ";
+        getline(cin, aadharNumber);
+
+        cout << "Blood Group: ";
+        getline(cin, bloodGroup);
+
+        cout << "Contact Address: ";
+        getline(cin, contactAddress);
+
+        cout << "Telephone Number: ";
+        getline(cin, telephoneNumber);
+
+        cout << "Percentage Marks: ";
+        cin >> percentageMarks;
+        cin.get(); // Consume the newline character left in the buffer
+
+        students[i].setData(name, rollNumber, studentClass, division, dob, aadharNumber, bloodGroup, contactAddress, telephoneNumber, percentageMarks);
+        cout << endl;
+    }
+
+    cout << "Student Information: " << endl;
+    for (int i = 0; i < numStudents; ++i) {
+        Student::displayStudentInfo(students[i]);
+        displayPercentageMarks(students[i]);
+        cout << endl;
+    }
+
+    cout << "Total number of students: " << Student::getCount() << endl;
+
+    return 0;
+}
